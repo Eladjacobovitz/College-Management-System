3a299b5c9823d585eedbb882453e83f5d5d57e3a
Elad Jacobovitz <62474541+Eladjacobovitz@users.noreply.github.com>
2020-03-23 18:26:07 +0200

    Update CourseTakenServiceTests.java

--- src/College_Management_System/Service/CourseTakenServiceTests.java
+++ src/College_Management_System/Service/CourseTakenServiceTests.java
@@ -4,13 +4,13 @@ public class CourseTakenServiceTests {
 
 	public static void main(String[] args) {
 		
-		//  Builds some demo courses.
+		// Builds some demo courses.
 		CourseService course1 = new CourseService(1, "Machine_Learning");
 		CourseService course2 = new CourseService(2, "Lineare_Algebra");
 		CourseTakenService coursetaken1 = new CourseTakenService(course1, 1);
 		CourseTakenService coursetaken2 = new CourseTakenService(course2, 2);
 		
-		//We will define a new "taken course" and activate the existing functions on it
+		// We will define a new "taken course" and activate the existing functions on it
 		CourseService tempcourse = coursetaken1.getCourse();
 		System.out.println(tempcourse.toString());
 		System.out.println(coursetaken2.getGrade());
